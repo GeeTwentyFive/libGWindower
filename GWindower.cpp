08fcbd75d0ece8983c233bca0e0dfa17f8950d0c
GeeTwentyFive <alickoamarko@gmail.com>
2026-08-12 21:31:43 +0200

    WIP

--- GWindower.cpp
+++ GWindower.cpp
@@ -99,7 +99,7 @@ bool GWindower::Update(
         if (_last_mouse_lock_state != locked_mouse) { _last_mouse_lock_state = locked_mouse;
                 if (locked_mouse) {
                         glfwSetInputMode((GLFWwindow*)_glfwWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED); if (glfwRawMouseMotionSupported()) glfwSetInputMode((GLFWwindow*)_glfwWindow, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
-                        glfwSetCursorPos((GLFWwindow*)_glfwWindow, this->window_width/2.0, this->window_height/2.0);  // (to prevent jerking/snapping)
+                        glfwSetCursorPos((GLFWwindow*)_glfwWindow, this->window_width, this->window_height);  // (to prevent jerking/snapping)
                 }
                 else { glfwSetInputMode((GLFWwindow*)_glfwWindow, GLFW_CURSOR, GLFW_CURSOR_NORMAL); }
         }
